264383b48a72cee7e1a696660f9579649d5a0142
Uku-Kaspar Uustalu <uku-kaspar.uustalu@tufts.edu>
2026-04-30 14:43:41 -0400

    autoformat via rubocop

--- lib/model_discovery.rb
+++ lib/model_discovery.rb
@@ -1,4 +1,6 @@
-class ModelDiscovery
+# frozen_string_literal: true
+
+class ModelDiscovery # rubocop:disable Style/Documentation
   def initialize(entries)
     @entries = entries
   end
@@ -9,28 +11,27 @@ class ModelDiscovery
         m[:label],
         m[:model_path],
         {
-          "data-set-model-file"  => m[:model_path],
-          "data-set-mmproj-file" => m[:mmproj_path]
+          'data-set-model-file' => m[:model_path],
+          'data-set-mmproj-file' => m[:mmproj_path]
         }
       ]
     end
   end
 
   def first_model_path
-    @entries.first&.fetch(:model_path, "")
+    @entries.first&.fetch(:model_path, '')
   end
 
   def first_mmproj_path
-    @entries.first&.fetch(:mmproj_path, "")
+    @entries.first&.fetch(:mmproj_path, '')
   end
 end
 
-
 def mmproj?(filename)
-  File.basename(filename).start_with?("mmproj")
+  File.basename(filename).start_with?('mmproj')
 end
 
-def discover_models(base_dir)
+def discover_models(base_dir) # rubocop:disable Metrics
   return ModelDiscovery.new([]) unless Dir.exist?(base_dir)
 
   entries = []
@@ -38,22 +39,22 @@ def discover_models(base_dir)
   Dir.children(base_dir).sort.each do |entry|
     full_path = File.join(base_dir, entry)
 
-    if File.file?(full_path) && entry.end_with?(".gguf")
+    if File.file?(full_path) && entry.end_with?('.gguf')
       entries << {
-        label:       File.basename(entry, ".gguf"),
-        model_path:  full_path,
-        mmproj_path: "",
+        label: File.basename(entry, '.gguf'),
+        model_path: full_path,
+        mmproj_path: ''
       }
     elsif File.directory?(full_path)
-      gguf_files = Dir.glob(File.join(full_path, "*.gguf")).sort
+      gguf_files = Dir.glob(File.join(full_path, '*.gguf')).sort
       model_files, mmproj_files = gguf_files.partition { |f| !mmproj?(f) }
 
       next if model_files.empty?
 
       entries << {
-        label:       entry,
-        model_path:  model_files.first,
-        mmproj_path: mmproj_files.first || "",
+        label: entry,
+        model_path: model_files.first,
+        mmproj_path: mmproj_files.first || ''
       }
     end
   end
